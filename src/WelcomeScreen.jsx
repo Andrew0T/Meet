8874d627881cc9b2fd2a14ed9a0fb4aab1931b9e
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-03-30 11:59:35 +0200

    commit title

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -3,16 +3,16 @@ import './WelcomeScreen.css';
 
 function WelcomeScreen(props) {
   return props.showWelcomeScreen ? (
-    <div className="WelcomeScreen">
+    <div class="WelcomeScreen">
     <h1> Welcome to the Meet app</h1>
     <h4>
     Log in to see upcoming events around the world for full-stack developers
     </h4>
-      <div className="button_cont" align="center">
-        <div className="google-btn">
-          <div className="google-icon-wrapper">
+      <div class="button_cont" align="center">
+        <div class="google-btn">
+          <div class="google-icon-wrapper">
             <img
-              className="google-icon"
+              class="google-icon"
               src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
               alt="Google sign-in"
             />
@@ -20,7 +20,7 @@ function WelcomeScreen(props) {
         <button 
           onClick={() => { props.getAccessToken() }}
           rel="nofollow noopener"
-          className="btn-text"
+          class="btn-text"
         >
         <b>Sign in with google</b>
         </button>
