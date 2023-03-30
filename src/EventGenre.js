93bfde500badf2d81022ba2a2cadfd88d2d95485
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-03-30 11:49:57 +0200

    commit title

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -17,7 +17,7 @@ const EventGenre = ({ events }) => {
 }, [events]);
   
 
-  const colors = ["#516D7F", "#000000", "#BEBE00", "#FFFFFF", "#02219B"];
+  const colors = ["#B1FF6D", "#000000", "#BEBE00", "#FFFFFF", "#02219B"];
 
   return (
     <ResponsiveContainer width="100%" height={400}>
