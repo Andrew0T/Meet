8fa1eac76850264adc6264ed0821831d9b37801b
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-04-04 18:17:32 +0200

    commit title

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -1,45 +1,40 @@
-import React, { useEffect, useState } from 'react';
-import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
+import React, { useEffect, useState } from "react";
+import { PieChart, Pie, Cell, ResponsiveContainer } from "recharts";
 
 const EventGenre = ({ events }) => {
+  const [data, setData ] = useState([]);
+
   useEffect(() => {
-    setData(() => getData());
-  }, [events]);
-
-  const [data, setData] = useState([]);
-
-  const getData = () => {
+    setData(() => {
     const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
-    const eventGenres = genres.map((genre) => {
+    const data = genres.map((genre) => {
       const value = events.filter((event) =>
-        event.summary.split(' ').includes(genre)
-      ).length;
+        event.summary.split(' ').includes(genre)).length;
       return { name: genre, value };
     });
-    const data = eventGenres.filter((genre) => genre.value !== 0);
-    return data;
-  };
+    return data.filter((genre) => genre.value !== 0);
+  });
+}, [events]);
+  
 
   const colors = ["#B1FF6D", "#000000", "#BEBE00", "#FFFFFF", "#02219B"];
 
-  
   return (
-    <ResponsiveContainer height={400}>
-      <PieChart width={400} height={400}>
+    <ResponsiveContainer width="100%" height={400}>
+      <PieChart width="50%" height="50%">
         <Pie
           data={data}
-          cx={200}
-          cy={200}
+          cx="50%"
+          cy="50%"
           labelLine={false}
           outerRadius={80}
-          fill="#8884d8"
           dataKey="value"
           label={({ name, percent }) =>
             `${name} ${(percent * 100).toFixed(0)}%`
           }
         >
           {data.map((_entry, index) => (
-            <Cell key={`cell-${index}`} fill={colors[index]} />
+            <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
           ))}
         </Pie>
       </PieChart>
