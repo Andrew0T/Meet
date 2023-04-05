ea4af7f48a6dbbba81ef21aa3c0cc9cb47c5556f
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-04-05 09:37:29 +0200

    commit title

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -2,20 +2,20 @@ import React, { useEffect, useState } from "react";
 import { PieChart, Pie, Cell, ResponsiveContainer } from "recharts";
 
 const EventGenre = ({ events }) => {
-  useEffect(() => { setData(() => getData());
+  useEffect(() => {
+    setData(() => getData());
   }, [events]);
 
   const [data, setData ] = useState([]);
 
   const getData = () => {
-    const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
-    const eventsData = genres.map((genre) => {
-      const value = events.filter((event) => event.summary.split(' ').includes(genre)).length;
-      return { name: genre, value };
-    });
-    const data = eventsData.filter((genre) => genre.value !== 0).length;
-      return data;
-  };
+    const genres = ["React", "JavaScript", "Node", "jQuery", "AngularJS"];
+    const data = genres.map((genre) =>{
+      const value = events.filter((genre) => events.summary.split(" ").includes(genre)).length
+      return {name: genre, value};
+    })
+    return data;
+  }
 
   const colors = ["#B1FF6D", "#000000", "#BEBE00", "#FFFFFF", "#02219B"];
 
@@ -28,11 +28,10 @@ const EventGenre = ({ events }) => {
           cy={200}
           labelLine={false}
           outerRadius={80}
-          dataKey="value"
           label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
         >
-          {data.map((_entry, index) => (
-            <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+          {data.map((index) => (
+            <Cell key={`cell-${index}`} fill={colors[index]} />
           ))}
         </Pie>
       </PieChart>
